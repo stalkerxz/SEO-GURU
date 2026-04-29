2fd887b0b6b677ae23a3abf77432f4df18542118
stalkerxz <stalkerxz@bk.ru>
2026-04-30 03:37:12 +0500

    chore: restore typed pg import in api entrypoint

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -5,7 +5,7 @@ import { randomUUID } from 'crypto';
 import path from 'path';
 import fs from 'fs';
 import { Readable } from 'stream';
-const { Pool } = require('pg');
+import { Pool } from 'pg';
 import Redis from 'ioredis';
 import { S3Client, PutObjectCommand, CreateBucketCommand, GetObjectCommand } from '@aws-sdk/client-s3';
 import dotenv from 'dotenv';
